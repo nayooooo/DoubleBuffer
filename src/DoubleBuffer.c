05b33dbfdda05316b3d0169fb466b362874ed080
nayooooo <YEWANhup@outlook.com>
2025-07-26 17:32:42 +0800

    1

--- src/DoubleBuffer.c
+++ src/DoubleBuffer.c
@@ -1,1 +1,70 @@
+/**
+ * @file DoubleBuffer.c
+ */
+
 #include "DoubleBuffer.h"
+
+#define db_set_event(db, evt) db->event |=  evt
+#define db_clr_event(db, evt) db->event &= ~evt
+
+int db_init(struct DoubleBuffer * const db)
+{
+    if (db == nullptr) {
+        return -1;
+    }
+
+    db->state = DB_STATE_UNINIT;
+
+    db->buff[0] = nullptr;
+    db->buff[1] = nullptr;
+    db->size = 0;
+    db->counter[0] = 0;
+    db->counter[1] = 0;
+    db->back_index = 0;
+
+    db->state |= DB_STATE_NO_BUFFER;
+
+    db->event = DB_EVENT_NONE;
+
+    return 0;
+}
+
+int db_set_buffer(
+    struct DoubleBuffer * const db,
+    uint8_t * const buf0, uint8_t * const buf1,
+    const uint32_t size
+)
+{
+    if (db == nullptr) {
+        return -1;
+    }
+    if (buf0 == nullptr || buf1 == nullptr) {
+        return -2;
+    }
+    if (size <= 0) {
+        return -3;
+    }
+
+    db->buff[0] = buf0;
+    db->buff[1] = buf1;
+    db->size = size;
+
+    db->state &= ~DB_STATE_NO_BUFFER;
+    db->state |= DB_STATE_IDLE;
+
+    return 0;
+}
+
+int db_set_send_handle(
+    struct DoubleBuffer * const db,
+    uint32_t (*send)(uint8_t *buf, uint32_t size)
+)
+{
+    if (db == nullptr) {
+        return -1;
+    }
+
+    db->send = send;
+
+    return 0;
+}
--- src/DoubleBuffer.h
+++ src/DoubleBuffer.h
@@ -1,6 +1,126 @@
+/**
+ * @file DoubleBuffer.h
+ */
+
 #ifndef __DOUBLEBUFFER_H__
 #define __DOUBLEBUFFER_H__
 
 #include <stdint.h>
+
+/*==================================================
+    Tools Begin
+==================================================*/
+
+/* ----- Bit ----- */
+
+#ifndef BIT
+#   define BIT(n) (1UL << (n))
+#endif  // !BIT
+
+/* ----- Pointer ----- */
+
+#ifndef nullptr
+#   define nullptr ((void *)0)
+#endif  // !nullptr
+
+/* ----- Structer ----- */
+
+#ifndef container_of
+#   define container_of(ptr, type, member) \
+        ((type *)((char *)(ptr) - (unsigned long)(&((type *)0)->member)))
+#endif  // !container_of
+
+/*==================================================
+    Tools End
+==================================================*/
+
+/*==================================================
+    Fsm Begin
+==================================================*/
+
+typedef uint32_t DB_State;
+#define DB_STATE_UNINIT                 0
+#define DB_STATE_NO_BUFFER              BIT(0)
+#define DB_STATE_IDLE                   BIT(1)
+#define DB_STATE_FILLING                BIT(2)
+#define DB_STATE_FILLED                 BIT(3)
+#define DB_STATE_SENDING                BIT(4)
+#define DB_STATE_SENT                   BIT(5)
+#define DB_STATE_READY_TO_SWAP          BIT(6)
+#define DB_STATE_SWAPPING               BIT(7)
+#define DB_STATE_MASK                   (BIT(8) - 1)
+
+/*==================================================
+    Fsm End
+==================================================*/
+
+/*==================================================
+    Event Begin
+==================================================*/
+
+typedef uint32_t DB_Event;
+#define DB_EVENT_NONE                   0
+#define DB_EVENT_START_FILL             BIT(0)
+#define DB_EVENT_STOP_FILL              BIT(1)
+#define DB_EVENT_START_SEND             BIT(2)
+#define DB_EVENT_STOP_SEND              BIT(3)
+#define DB_EVENT_SWAP                   BIT(4)
+#define DB_EVENT_MASK                   (BIT(5) - 1)
+
+/*==================================================
+    Event End
+==================================================*/
+
+/*==================================================
+    Structer Begin
+==================================================*/
+
+/* ----- DoubleBuffer ----- */
+
+struct DoubleBuffer
+{
+    /*---------- buf ----------*/
+
+    uint8_t *buff[2];
+    uint32_t size;
+    volatile uint32_t counter[2];
+
+    volatile uint8_t back_index;
+
+    /*---------- fsm ----------*/
+
+    volatile DB_State state;
+
+    /*--------- event ---------*/
+
+    volatile DB_Event event;
+
+    /*-------- handle ---------*/
+
+    uint32_t (*send)(uint8_t *buf, uint32_t size);
+};
+
+/*==================================================
+    Structer End
+==================================================*/
+
+/*==================================================
+    API Begin
+==================================================*/
+
+int db_init(struct DoubleBuffer * const db);
+int db_set_buffer(
+    struct DoubleBuffer * const db,
+    uint8_t * const buf0, uint8_t * const buf1,
+    const uint32_t size
+);
+int db_set_send_handle(
+    struct DoubleBuffer * const db,
+    uint32_t (*send)(uint8_t *buf, uint32_t size)
+);
+
+/*==================================================
+    API End
+==================================================*/
 
 #endif  /* __DOUBLEBUFFER_H__ */
